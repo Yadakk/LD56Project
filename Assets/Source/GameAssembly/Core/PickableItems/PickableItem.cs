42671a45ff44c8e1d6041576b4fb78ea743449b9
Yad <lolyer47@gmail.com>
2024-10-05 12:43:51 +0300

    feat: Items can be picked now

--- Assets/Source/GameAssembly/Core/PickableItems/PickableItem.cs
+++ Assets/Source/GameAssembly/Core/PickableItems/PickableItem.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityServiceLocator;
 
 namespace LD56Project.GameAssembly
 {
@@ -9,11 +10,19 @@ namespace LD56Project.GameAssembly
         [SerializeField]
         private ItemData itemData;
 
+        private Inventory inventory;
+
         public string Text => "Pick up";
+
+        private void Start()
+        {
+            inventory = ServiceLocator.ForSceneOf(this).Get<Inventory>();
+        }
 
         public void Interact()
         {
-            Debug.Log(itemData.name);
+            if (!inventory.TryAddItem(itemData)) return;
+            Destroy(gameObject);
         }
     }
 }
